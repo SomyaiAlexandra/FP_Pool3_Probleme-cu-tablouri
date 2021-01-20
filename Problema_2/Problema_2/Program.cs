52c7e47cc82ad3f2b8596a130c98e8659e111c02
Alexandra Somyai <somyai.alexandra@yahoo.com>
2021-01-20 18:47:47 +0200

    Finish code.

---
+++ Problema_2/Problema_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_2
+{
+    class Program
+    {
+        // Se da un vector cu n elemente si o valoare k. 
+        // Se cere sa se determine prima pozitie din vector pe care apare k. 
+        // Daca k nu apare in vector rezultatul va fi -1. 
+        static void Main(string[] args)
+        {
+            int n, k;
+            Console.Write("n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("k: ");
+            k = int.Parse(Console.ReadLine());
+
+            int[] v = new int[n];
+            //initializare valori vector
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+
+            }
+            bool ok = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (v[i] == k)
+                {
+                    Console.WriteLine($"Valoarea {k} se afla pe pozitia {i}");
+                    ok = true;
+                    break;
+                }
+            }
+
+            if (ok == false)
+                Console.WriteLine(-1);
+        }
+    }
+}
